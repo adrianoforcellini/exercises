01f387a7de1352de64636ddc468c24467836e7f4
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-01 01:43:16 -0300

    exercício1

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+function imprimaQuadrado(n) {
+    let linha = "";
+
+    for (let a = 0; a <= n; a++) {
+        if (linha.length == n)
+            for (let b = 0; b <= n; b++) {
+                console.log(linha);
+            }
+
+        else
+            linha += "*";
+    }
+
+}
