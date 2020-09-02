1854a07e4250e44d3855de0c327a8543e038b10e
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-01 22:18:38 -0300

    adicionando comentarios

--- exercicio6.js
+++ exercicio6.js
@@ -1,4 +1,4 @@
 function ehPalindromo(palavra) {
-    let aocontrario = palavra.split('').reverse().join('');
-    return palavra === aocontrario;
+    let aocontrario = palavra.split('').reverse().join(''); //str-> array -> array^(-1) -> str^(-1)
+    return palavra === aocontrario; // retorna true se str = str^(-1)
 }
