824ca97a846e50d80269dc519123214722c19bb0
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-19 19:09:05 -0300

    ex6

---
+++ js4v2.js
@@ -0,0 +1,4 @@
+function textCorrector() {
+    document.getElementsByTagName("p")[0].style.textOverflow = "ellipsis";
+    document.getElementsByTagName("p")[0].style.textAlign = "center";
+}
---
+++ js6v2.js
@@ -0,0 +1,8 @@
+function allPTags() {
+    let tagsP = document.getElementsByTagName('s');
+    for (let i = 0; i < tagsP.length; i++) {
+        console.log(tagsP[i]);
+    }
+
+
+}
