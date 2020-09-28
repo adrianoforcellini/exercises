41d6704864c7d27c6505e9fff41b7505ee583641
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-28 00:42:45 -0300

    pt2ex1 - criando arrow function que recebe um número e retorna seu fatorial

---
+++ ex2.js
@@ -0,0 +1,7 @@
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
+console.log(s)
---
+++ pt2ex1.js
@@ -0,0 +1,8 @@
+const fator = (n) => {
+let soma = 0;
+for ( i=1; i <= n ; i++){
+soma += n*i 
+
+}
+return soma
+}
