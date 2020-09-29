f89257b92e7fba30e36e3be23ceb296c6882d3c8
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-28 22:26:34 -0300

    conta-cliques

--- pt2ex2.js
+++
@@ -1,1 +0,0 @@
-const biggestWord = (str) => {let arr = str.split(" ");let longest = '';for (let i = 0; i < arr.length ; i++){if (longest < arr[i].length) {longest = arr[i].length; word = arr[i];}}return word;}
